e31c5ef111d9d184838bffd16d226a0843757b91
Hyfos2 <bthighforce@gmail.com>
2018-11-12 05:56:35 +0200

    error fxed

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -7,18 +7,19 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
+    
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check() && Auth::user()->userTypeId  ==1) {
             return redirect('/admin-dashboard');
+        }
+
+        if (Auth::guard($guard)->check() && Auth::user()->userTypeId  ==4) {
+            return redirect('/accounts-dashboard');
+        }
+
+        if (Auth::guard($guard)->check() && Auth::user()->userTypeId  ==5) {
+            return redirect('/education-dashboard');
         }
         if(Auth::guard($guard)->check() && Auth::user()->userTypeId  ==2) {
             return redirect('/welcome');
